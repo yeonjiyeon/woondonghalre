244ba063bcf1ed0763c777c1755ac8f0a5f61df0
yeonjiyeon <dalnim95@naver.com>
2022-05-24 18:06:37 +0900

    no message

---
+++ back/woondonghalre/src/main/java/woondonghalre/woondonghalre/WoondonghalreApplication.java
@@ -0,0 +1,13 @@
+package woondonghalre.woondonghalre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoondonghalreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoondonghalreApplication.class, args);
+	}
+
+}
---
+++ back/woondonghalre/src/test/java/woondonghalre/woondonghalre/WoondonghalreApplicationTests.java
@@ -0,0 +1,13 @@
+package woondonghalre.woondonghalre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoondonghalreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
