92846862c0f3756e871d88b91aa7ae415a41e6de
yeonjiyeon <dalnim95@naver.com>
2022-06-15 18:39:52 +0900

    init back

---
+++ back/wondonghalre/src/main/java/com/wondong/halre/wondonghalre/WondonghalreApplication.java
@@ -0,0 +1,13 @@
+package com.wondong.halre.wondonghalre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WondonghalreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WondonghalreApplication.class, args);
+	}
+
+}
---
+++ back/wondonghalre/src/test/java/com/wondong/halre/wondonghalre/WondonghalreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wondong.halre.wondonghalre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WondonghalreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
